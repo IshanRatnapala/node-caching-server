933122998848997f35c71dac5cd602592fbb8e09
Dragfreeze <Dragfreeze@gmail.com>
2017-03-17 00:21:56 +0530

    added redis

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const express = require('express');
 const request = require('superagent');
 const PORT = process.env.PORT || 3000;
 
+const redis = require('redis');
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
 const app = express();
+const client = redis.createClient(REDIS_PORT);
 
 function respond (org, numberOfRepos) {
     return `org ${org} has ${numberOfRepos} public reps`;
@@ -16,13 +20,27 @@ function getNumberOfRepos (req, res, next) {
         if (err) throw err;
 
         let repoNumber = response.body.length;
+
+        client.setex(org, 10, repoNumber);
         res.send(respond(org, repoNumber));
     });
 }
 
-app.get('/repos', getNumberOfRepos);
+function cache (req, res, next) {
+    const org = req.query.org;
+    client.get(org, function (err, data) {
+        if (err) throw err;
 
-console.log(PORT);
+        if (data != null) {
+            res.send(respond(org, data));
+        } else {
+            next();
+        }
+    })
+}
+
+app.get('/repos', cache, getNumberOfRepos);
+
 app.listen(PORT, function () {
-    console.log('Server started on port'. PORT);
+    console.log('Server started on port', PORT);
 });
